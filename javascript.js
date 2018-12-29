1dcbab15a2a105198f0e098524f7085e2c813169
Jimmy Chan <jimmychan018@gmail.com>
2018-12-30 11:38:37 +1300

    Files Uplodaed
    Added HTML/CSS/JS Files

---
+++ javascript.js
@@ -0,0 +1,12 @@
+function displayResult() {
+    document.getElementById("sizeID").innerHTML = "S";
+}
+
+  function displayResult1() {
+    document.getElementById("sizeID").innerHTML = "M";
+}
+
+  function displayResult2() {
+    document.getElementById("sizeID").innerHTML = "L";
+  }
+
